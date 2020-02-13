1a60984046a33baa44260ea1047b9dfefdebc811
Pavel <36270842+pvlplv@users.noreply.github.com>
2020-02-13 22:21:01 +0300

    static file handling error fix

--- routes.py
+++ routes.py
@@ -1,8 +1,12 @@
 from views import (about_view, artist_view, artists_view, event_view,
                    events_view, index_view, release_view, releases_view)
+from pathlib import Path
 
+STATIC_DIR = Path(__file__).parent / 'static'
 
 def setup_routes(app):
+    app.router.add_static('/static/', STATIC_DIR, name='static')
+    
     app.router.add_get('/', index_view, name='index')
     app.router.add_get('/about', about_view, name='about')
 
@@ -14,3 +18,7 @@ def setup_routes(app):
 
     app.router.add_get('/events', events_view, name='events')
     app.router.add_get('/events/{route}', event_view)
+
+    # app.router.add_get('/items', items_view, name='items')
+    # app.router.add_get('/items/{route}', item_view)
+    # app.router.add_get('/items/{route}/{id}', item_view)
